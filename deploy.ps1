f5ffd6b679b90d050055000455d2af5066581d7e
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-12-26 22:47:22 +0100

    Added missing semicolon again

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
 if($env:appveyor_repo_tag -eq 'True') {
-    Write-Host "Deploying to NPM"
+    Write-Host "Deploying to NPM";
     $npm_token = $env:NPM_TOKEN;
     "//registry.npmjs.org/:_authToken=$($npm_token)" | Out-File .npmrc;
     npm publish .;
     Remove-Item .npmrc;
 } else {
-    Write-Host "Skipping deployment to NPM"
+    Write-Host "Skipping deployment to NPM";
 }
